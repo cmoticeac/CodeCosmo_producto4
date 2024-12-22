882bca3c622280c2ec9f08aa3424090a4d86bac8
cbravose <cbravose@uoc.edu>
2024-12-22 23:04:47 +0100

    _comit listado

--- app/src/listado.js
+++ app/src/listado.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { FlatList, View, Text,  Button, Image, ActivityIndicator } from 'react-native';
-import { database, ref, onValue } from './firebase/firebase.js'; // Aquí importamos ref y onValue
-import Detalle from './detalle'; // Asegúrate de importar el componente Detalle
+import { database, ref, onValue } from './firebase/firebase.js';  
+import Detalle from './detalle';  
 import Mensajes from './firebase/mensajes.js';
 import Mystyles from './mystyles.js';
 
-// Mensajes();  //mensajes token
+ 
 class Listado extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jugadores: [], // Estado para almacenar los jugadores
-      selectedJugador: null, // Jugador seleccionado para mostrar detalles
-      loading: false, // Indicador de carga
-      page: 1, // Página actual (sirve para saber cuántas veces agregar los 10 jugadores)
-      allJugadores: [], // Almacenaremos todos los jugadores en un solo arreglo para evitar duplicados
+      jugadores: [], 
+      selectedJugador: null,  
+      page: 1,  
+      allJugadores: [],  
     };
   }
 
   componentDidMount() {
-    this.loadJugadores(); // Cargar los jugadores cuando el componente se monta
+    this.loadJugadores();  
   }
 
-  // Cargar los jugadores de la base de datos
+ 
   loadJugadores = () => {
     const jugadoresRef = ref(database, '/jugadores');
 
@@ -37,12 +36,12 @@ class Listado extends React.Component {
           ...value,
         }));
 
-        // Guardamos los primeros 10 jugadores
+    
         const primeros10Jugadores = jugadoresArray.slice(0, 10);
 
         this.setState({
-          jugadores: primeros10Jugadores, // Establecemos los primeros 10 jugadores
-          allJugadores: primeros10Jugadores, // Almacenamos todos los jugadores
+          jugadores: primeros10Jugadores,  
+          allJugadores: primeros10Jugadores,  
           loading: false,
         });
       } else {
@@ -51,20 +50,19 @@ class Listado extends React.Component {
     });
   };
 
-  // Función que se ejecuta cuando el usuario llega al final de la lista
+  
   handleEndReached = () => {
     const { loading, jugadores, allJugadores, page } = this.state;
 
-    // Si estamos cargando o ya no hay más jugadores para agregar, no hacemos nada
+    
     if (loading) return;
 
     this.setState(
       (prevState) => ({
-        page: prevState.page + 1, // Aumentamos la página
-        loading: true, // Activamos el indicador de carga
+        page: prevState.page + 1, 
       }),
       () => {
-        // Crear una nueva lista de jugadores duplicados con un ID único
+        
         const jugadoresRepetidos = jugadores.map((jugador, index) => ({
           ...jugador,
           id: `${jugador.id}_duplicado_${allJugadores.length + index}`, // Asignamos un nuevo ID único basado en el índice total
@@ -73,13 +71,13 @@ class Listado extends React.Component {
         this.setState((prevState) => ({
           jugadores: [...prevState.jugadores, ...jugadoresRepetidos], // Añadimos los mismos jugadores al final
           allJugadores: [...prevState.allJugadores, ...jugadoresRepetidos], // Actualizamos la lista de todos los jugadores
-          loading: false, // Desactivamos el indicador de carga
+          loading: false,  
         }));
       }
     );
   };
 
-// inicialmente se puede controlar token al clic para list o update...
+ 
   handlePress = (item) => {
     this.setState({ selectedJugador: item });
   };
@@ -91,7 +89,7 @@ class Listado extends React.Component {
     <View style={Mystyles.item}>
 
       <Image
-        source={{ uri: item.img1 }} // Suponiendo que `item.img1` contiene la URL de la imagen
+        source={{ uri: item.img1 }}  
         style={Mystyles.image}
       />
       <Text style={Mystyles.name}>Nombre: {item.nombre}</Text>
@@ -114,9 +112,9 @@ class Listado extends React.Component {
           <FlatList
             data={jugadores}
             renderItem={this.renderItem}
-            keyExtractor={(item) => item.id} // Aquí usamos la clave única
-            onEndReached={this.handleEndReached} // Cargar más jugadores al llegar al final
-            onEndReachedThreshold={0.1} // Activar cuando el usuario está al 10% del final
+            keyExtractor={(item) => item.id}  
+            onEndReached={this.handleEndReached}  
+            onEndReachedThreshold={0.1}  
             ListFooterComponent={loading ? <ActivityIndicator size="large" color="#0000ff" /> : null} // Mostrar cargando
           />
         )}
